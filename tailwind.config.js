6d255c8f02c783537b2af5e13538440e6d7d9d4c
Yifei Fang <yifeifang@Yifeis-MacBook-Air.local>
2023-11-04 16:50:45 -0500

    initial commit, draft portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        greybg: "#f5f4f4",
+        bluetext: "rgba(181, 234, 215, 0.3)",
+        purpletext: "rgba(199, 206, 234, 0.3)",
+        grey_transparent: "rgba(217, 217, 217, 0.3)",
+        bluetext_dark: "rgba(181, 234, 215, 0.7)",
+        purpletext_dark: "rgba(199, 206, 234, 0.7)",
+      },
+    },
+  },
+  plugins: [],
+};
